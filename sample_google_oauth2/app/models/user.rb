3bdf44b20e61e9e68b3575b59a6b51bef28d580a
togana <o.togana@gmail.com>
2016-12-13 00:38:48 +0900

    fix devise user

--- sample_google_oauth2/app/models/user.rb
+++ sample_google_oauth2/app/models/user.rb
@@ -1,6 +1,18 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+class User < ActiveRecord::Base
+  devise :trackable, :omniauthable, omniauth_providers: %i(google)
+
+  protected
+  def self.find_for_google(auth)
+    user = User.find_by(email: auth.info.email)
+
+    unless user
+      user = User.create(name:     auth.info.name,
+                         provider: auth.provider,
+                         uid:      auth.uid,
+                         token:    auth.credentials.token,
+                         password: Devise.friendly_token[0, 20],
+                         meta:     auth.to_yaml)
+    end
+    user
+  end
 end
